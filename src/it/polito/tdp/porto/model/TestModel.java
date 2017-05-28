8942e4a5cb3ad0ef71cfa65026e09639693100c9
Benedetta Giannantoni <Ben@10.7.18.233>
2017-05-28 10:10:05 +0200

    Ex1

--- src/it/polito/tdp/porto/model/TestModel.java
+++ src/it/polito/tdp/porto/model/TestModel.java
@@ -8,11 +8,8 @@ public class TestModel {
 		Author a1=model.getAuthorById(1859);
 		Author a2=model.getAuthorById(2041);
 		Author a3=model.getAuthorById(85);
-		Paper p=model.getArticoloDatoCodice(2497280);
-		//model.createGraph();
-//		model.getCo_Authors(a1);
-		//System.out.println(model.getPaperAuthors(2540290));
-		System.out.println(a2);
+
+		System.out.println(a2); 
 		System.out.println(model.getCo_Authors(a2));
 	
 	}
